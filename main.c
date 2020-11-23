b368a7432fa8fd08e3aff24f769214add1d51186
houngju07 <ohyoungju@sookmyung.ac.kr>
2020-11-23 20:04:37 +0900

    12_04
    12_04

--- main.c
+++ main.c
@@ -5,12 +5,9 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void main(int argc, char *argv[]) {
-	char src[]="The worst things to eat before you sleep";
-	char dst[100];
+	char str[30]="happy C prodramming";
 	
-	strcpy(dst,src);
-	
-	printf("copied string: %s",dst);
+	printf("length of \"%s\" :%i",str,strlen(str));
 	
 	return 0;
 }
